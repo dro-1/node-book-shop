ee931099de8ab87267f5c992e25209ba9d7fe79e
dro-1 <lorddro1532@gmail.com>
2020-12-31 11:51:00 +0100

    Added templating engine syntax

--- app.js
+++ app.js
@@ -2,6 +2,11 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const path = require("path");
 const app = express();
+const expressHbs = require("express-handlebars");
+
+app.engine("hbs", expressHbs());
+app.set("view engine", "hbs");
+app.set("views", "views");
 
 const adminData = require("./routes/admin");
 const shopRouter = require("./routes/shop");
@@ -14,7 +19,7 @@ app.use("/admin", adminData.router);
 app.use(shopRouter);
 
 app.use((req, res) => {
-  res.status(404).sendFile(path.join(__dirname, "views", "404page.html"));
+  res.status(404).render("404", { pageTitle: "Page Not Found" });
 });
 
 app.listen(3000, "dro");
